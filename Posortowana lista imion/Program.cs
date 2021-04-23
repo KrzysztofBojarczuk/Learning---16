f27829e57e5c74b17aff92e8bf6d5d9c149398da
KrzysztofBojarczuk <69596796+KrzysztofBojarczuk@users.noreply.github.com>
2021-04-23 09:20:03 +0200

    Add project files.

---
+++ Posortowana lista imion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Posortowana_lista_imion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int SIZE = 5;
+            string[] names = new string[SIZE];
+
+            for (int i = 0; i <= SIZE-1; i++)
+            {
+                Console.WriteLine("Wprowadź imiona:");
+                names[i] = Console.ReadLine();
+            }
+            Array.Sort(names);
+            for (int i = 0; i <= SIZE-1; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+
+        }
+    }
+}
